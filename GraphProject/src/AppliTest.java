42e9a654eb05c881f39b38068925415e74e83fb6
Latoof <Latoof@192.168.5.142>
2011-10-12 23:39:54 +0200

    =)

--- GraphProject/src/AppliTest.java
+++ GraphProject/src/AppliTest.java
@@ -7,6 +7,7 @@ public class AppliTest {
 	public static void main(String[] args) {
 		System.out.println("Lol");
 		System.out.println("Gros phasme !");
+		System.out.println("Vieux deg !");
 	}
 
 }
