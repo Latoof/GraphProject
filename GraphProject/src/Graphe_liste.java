8dff6d637b47755dfb5a228149464ad26e572127
Anaguiros <matthieu.lenogue@gmail.com>
2011-10-19 15:42:14 +0200

    pour le vendéen.
    Signed-off-by: Anaguiros <matthieu.lenogue@gmail.com>

---
+++ GraphProject/src/Graphe_liste.java
@@ -0,0 +1,12 @@
+import java.util.LinkedList;
+import java.util.List;
+
+
+public class Graphe_liste {
+
+	List<Noeud> liste_noeud;
+	List<Arc>	liste_arc;
+	
+	LinkedList<Integer> matrice_adjacence;
+	
+}
