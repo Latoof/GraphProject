92ecf8887cc39e4deaef1b48557e6ee47db235ed
Anaguiros <matthieu.lenogue@gmail.com>
2011-11-09 17:18:07 +0100

    Signed-off-by: Anaguiros <matthieu.lenogue@gmail.com>

--- GraphProject/src/AppliTest.java
+++ GraphProject/src/AppliTest.java
@@ -9,7 +9,7 @@ public class AppliTest {
 	 */
 	public static void main(String[] args) {
 		
-		int t=1; // A modifier pour changer le test (je m'en sers aussi)
+		int t=0; // A modifier pour changer le test (je m'en sers aussi)
 		if (t == 0 ) {
 			Graphe_matrice graphe = new Graphe_matrice();
 			
@@ -73,7 +73,7 @@ public class AppliTest {
 			
 			System.out.println("Parcours en Profondeur");
 			graphe.parcoursProfondeur(n0);
-						
+			System.out.println("END");			
 		}
 		
 		else if ( t == 1 ) {
